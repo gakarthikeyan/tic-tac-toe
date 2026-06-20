9beac1a1101b7a2a83308c713ade84688e9bb784
Karthikeyan <karthiganesan.gk@gmail.com>
2026-06-20 23:17:32 +0530

    [Tic-Tac-Toe] Case is green when cell is empty and not empty when occupied.

--- app/src/test/java/com/kata/android/tictactoe/domain/model/GameBoardTest.kt
+++ app/src/test/java/com/kata/android/tictactoe/domain/model/GameBoardTest.kt
@@ -1,8 +1,10 @@
 package com.kata.android.tictactoe.domain.model
 
 import com.kata.android.tictactoe.utils.Constants.BOARD_CELL_COUNT
+import com.kata.android.tictactoe.utils.Constants.CELL_POSITION_ONE
 import com.kata.android.tictactoe.utils.Constants.CELL_POSITION_ZERO
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
@@ -20,5 +22,18 @@ class GameBoardTest {
         val gameBoard = GameBoard()
         val newBoard = gameBoard.markPlayerPlace(CELL_POSITION_ZERO, GamePlayer.X)
         assertEquals(GamePlayer.X, newBoard.cells[CELL_POSITION_ZERO])
+    }
+
+    @Test
+    fun `cell is empty when position is null`() {
+        val gameBoard = GameBoard()
+        assertTrue(gameBoard.isCellEmpty(CELL_POSITION_ONE))
+    }
+
+    @Test
+    fun `cell is not empty when occupied`() {
+        val gameBoard = GameBoard()
+        val newBoard = gameBoard.markPlayerPlace(CELL_POSITION_ONE, GamePlayer.O)
+        assertFalse(newBoard.isCellEmpty(CELL_POSITION_ONE))
     }
 }
