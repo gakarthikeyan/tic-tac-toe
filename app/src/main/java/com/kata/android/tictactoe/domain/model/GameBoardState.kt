efe36ab0c3c23d75e09ce5f5d3f710ba4ed039d7
Karthikeyan <karthiganesan.gk@gmail.com>
2026-06-21 13:48:45 +0530

    [Tic-Tac-Toe] Case is green for after current player move.

--- app/src/main/java/com/kata/android/tictactoe/domain/model/GameBoardState.kt
+++ app/src/main/java/com/kata/android/tictactoe/domain/model/GameBoardState.kt
@@ -5,4 +5,18 @@ data class GameBoardState(
     val currentPlayer: GamePlayer = GamePlayer.X,
     val isGameOver: Boolean = false,
     val gameBoardResult: GameBoardResult = GameBoardResult.ONGOING
-)
+) {
+
+    fun playMove(position: Int): GameBoardState {
+        if (isGameOver) return this
+        if (!gameBoard.isCellEmpty(position)) return this
+
+        val newBoard = gameBoard.markPlayerPlace(position, currentPlayer)
+        return GameBoardState(
+            gameBoard = newBoard,
+            currentPlayer = if (currentPlayer == GamePlayer.X) GamePlayer.O else GamePlayer.X,
+            isGameOver = false,
+            gameBoardResult = GameBoardResult.ONGOING
+        )
+    }
+}
--- app/src/test/java/com/kata/android/tictactoe/domain/model/GameBoardStateTest.kt
+++ app/src/test/java/com/kata/android/tictactoe/domain/model/GameBoardStateTest.kt
@@ -1,5 +1,6 @@
 package com.kata.android.tictactoe.domain.model
 
+import com.kata.android.tictactoe.utils.Constants.CELL_POSITION_ZERO
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
@@ -14,5 +15,12 @@ class GameBoardStateTest {
         assertTrue(gameBoardState.gameBoard.isBoardEmpty())
         assertFalse(gameBoardState.isGameOver)
         assertEquals(GameBoardResult.ONGOING, gameBoardState.gameBoardResult)
+    }
+
+    @Test
+    fun `after X moves, it's O's turn`() {
+        val gameBoardState = GameBoardState()
+        val newGameState = gameBoardState.playMove(CELL_POSITION_ZERO)
+        assertEquals(GamePlayer.O, newGameState.currentPlayer)
     }
 }
